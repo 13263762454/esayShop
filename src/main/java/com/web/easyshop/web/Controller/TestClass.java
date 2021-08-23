f9912817fcf3877c7239d864f00644ed048bb908
deni <1820746994@qq.com>
2021-08-23 17:05:49 +0800

    基础框架

---
+++ src/main/java/com/web/easyshop/web/Controller/TestClass.java
@@ -0,0 +1,26 @@
+package com.web.easyshop.web.Controller;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestClass {
+
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
